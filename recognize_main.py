9febc03d96d133eb76a8970f552e571a77a5b3fa
Gihun Ko <rhrlgns96@gmail.com>
2020-06-21 21:07:28 +0800

    added comments for better understanding

--- recognize_main.py
+++ recognize_main.py
@@ -7,8 +7,11 @@ import numpy as np
 known_image = face_recognition.load_image_file("dataset/hongchul/00000000.jpg")
 unknown_image = face_recognition.load_image_file("dataset/jaesuk/00000002.jpg")
 
+#Adding encoding for the known person 'hongchul'
 hongchul_encoding = face_recognition.face_encodings(known_image)[0]
 unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
 
+# We are comparing the unknown image with the known person 'hongchul'
 results = face_recognition.compare_faces([hongchul_encoding], unknown_encoding)
+#True: unkown matches the person 'hongchul', else False
 print(results)
