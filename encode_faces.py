573512688bf1d4908ae7f07a921b8b99c67ca22f
Gihun Ko <rhrlgns96@gmail.com>
2020-06-24 22:56:22 +0800

    added python script for encoding the image and save the serialized db

---
+++ encode_faces.py
@@ -0,0 +1,64 @@
+from imutils import paths
+import face_recognition
+import argparse
+import pickle
+import cv2
+import os
+import logging
+
+#ENCODING FACES
+#Developed: 24 June 2020
+#Developer: Ko Gi Hun
+
+#Initializing Logger
+logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+#Initializing arg parser
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--dataset", required=True, help = "path to input directory of faces + images")
+ap.add_argument("-e", "--encodings", required=True, help = "path to serialized db of facial encodings")
+ap.add_argument("-d", "--detection-method", type=str, default="cnn",
+                help = "face detection model to use: either 'hog' or 'cnn'")
+args = vars(ap.parse_args())
+
+if __name__ == "__main__":
+    logging.info('quantifying faces..')
+    imagePaths = list(paths.list_images(args["dataset"]))
+
+    #Initializing the list of known encodings and known names
+    knownEncodings = []
+    knownNames = []
+
+    #Looping through the dataset images for encoding
+    for (i, imagePath) in enumerate(imagePaths):
+        name = imagePath.split(os.path.sep)[-2]
+        logging.info("Processing image of {}: {}/{}".format(name, i + 1, len(imagePaths)))
+
+        #loading the image and convert it from BGR (Opencv ordering)
+        #To dlib ordering(RGB)
+        image = cv2.imread(imagePath)
+        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        #1. Detect the (x, y) coordinates of the bounding boxes
+        #corresponding to each face in the input image
+        boxes = face_recognition.face_locations(rgb, model = args["detection_method"])
+
+        #2. Compute the facial embedding for the face
+        encodings = face_recognition.face_encodings(rgb, boxes)
+
+        #3. Loop over the encodings
+        for encoding in encodings:
+            # Add encoding + name to our set of known names and encodings
+            knownEncodings.append(encoding)
+            knownNames.append(name)
+
+    # Serialization of encodings into file and save
+    logging.info("serializing encodings...")
+    data = {"encodings": knownEncodings, "names": knownNames}
+    f = open(args["encodings"], "wb")
+    f.write(pickle.dumpms(data))
+    f.close()
