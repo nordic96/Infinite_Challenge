7c5c4b260297b8a23f77a58b642024e6c741a3c4
Hu Yuchen <huyc1998@gmail.com>
2020-07-01 00:01:04 +0800

    manage testing and training resources

--- skull_detection.py
+++ skull_detection.py
@@ -1,4 +1,11 @@
-import torch
-from IPython.display import Image  # for displaying images
-# from utils.google_utils import gdrive_download  # for downloading models/datasets
-print('torch %s %s' % (torch.__version__, torch.cuda.get_device_properties(0) if torch.cuda.is_available() else 'CPU'))
+import subprocess
+
+YOLOV5_PATH = './dataset/skull/yolov5/'
+IMAGE_NUM = 10
+CONFIDENCE = 0.1
+
+subprocess.call(['pip', 'install', '-r', f'{YOLOV5_PATH}requirements.txt']) # setup yolov5 environment
+print("setup complete")
+
+subprocess.call(['python', f'{YOLOV5_PATH}detect.py', '--weights', f"{YOLOV5_PATH}weights/last_yolov5s_results.pt",
+                 '--img', f"{IMAGE_NUM}", '--conf', f"{CONFIDENCE}", '--source', "./dataset/skull/test_infer"])
