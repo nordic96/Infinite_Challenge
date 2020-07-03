040aaff13f9de367c033db21a33ae53cc1b228ee
Gi Hun Ko <rhrlgns96@gmail.com>
2020-07-03 13:56:57 +0800

    updates in SQL connector

--- utils/sql_connecter.py
+++ utils/sql_connecter.py
@@ -1,6 +1,8 @@
 import pyodbc
+import argparse
+import csv
 import configparser
-from logger.base_logger import logger
+import pandas as pd
 
 # Initialise strings from config file
 config = configparser.ConfigParser()
@@ -21,15 +23,46 @@ CONN_STRING = 'DRIVER={ODBC Driver 17 for SQL Server}' \
 # Developer: Ko Gi Hun
 
 
-class SQLConnector:
-    def __init__(self):
+class SqlConnector:
+    def __init__(self, file_path):
         self.conn = pyodbc.connect(CONN_STRING)
         self.cursor = self.conn.cursor()
+        self.file_path = file_path
 
-    def execute(self, query):
+    def execute(self, query, type):
         self.cursor.execute(query)
-        for row in self.cursor:
-            logger.info(row)
+        if type == 'SELECT':
+            for row in self.cursor:
+                print(row)
+        elif type == 'INSERT':
+            self.conn.commit()
 
-    def insertSkullInfo(self, csv_file_path):
+    def insert_skull_info(self):
+        with open(self.file_path, 'r') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                print(row)
         return None
+
+    def insert_skull_info_test(self, header):
+        df= pd.read_csv(self.file_path, header=header)
+        for index, row in df.iterrows():
+            print(row)
+
+        statement = "INSERT INTO skull VALUES ('{}', '{}', {});".format(row[0], row[1], row[2])
+        print(statement)
+        self.execute(statement, 'INSERT')
+        self.cursor.commit()
+        return None
+
+    def close(self):
+        self.conn.close()
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", required=True, help="input directory for csv file")
+args = vars(ap.parse_args())
+
+if __name__ == '__main__':
+    sqlconn = SqlConnector(args['input'])
+    sqlconn.insert_skull_info_test(None)
