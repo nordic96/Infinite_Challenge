d48da953b1eadf30468a1e27bb4e5257c5a31776
Gi Hun Ko <rhrlgns96@gmail.com>
2020-07-05 18:32:12 +0800

    updates in logger for saving log file

--- logger/base_logger.py
+++ logger/base_logger.py
@@ -1,4 +1,17 @@
 import logging
+from datetime import datetime
+import os
 
 logger = logging
-logger.basicConfig(format='%(asctime)s %(module)s [%(levelname)s] - %(message)s', level=logging.INFO)
+current_date = datetime.now().strftime('%YY_%m_%d')
+
+if not os.path.exists(current_date):
+    os.makedirs()
+
+current_datetime = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
+logfile_name = 'LOG_{}.log'.format(current_datetime)
+logger.basicConfig(
+    filename= '{}/{}'.format(current_date, logfile_name),
+    format='%(asctime)s %(module)s [%(levelname)s] - %(message)s',
+    level=logging.INFO
+)
