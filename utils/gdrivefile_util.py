778f8b0e4982bf627de151d05af4515504cb31e3
Gi Hun Ko <rhrlgns96@gmail.com>
2020-07-14 22:37:53 +0800

    added authenticate function for gdrive API

--- utils/gdrivefile_util.py
+++ utils/gdrivefile_util.py
@@ -12,6 +12,33 @@ from googleapiclient.http import MediaIoBaseDownload
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
 
 
+def authenticate():
+    """Shows basic usage of the Drive v3 API.
+    Prints the names and ids of the first 10 files the user has access to.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('drive', 'v3', credentials=creds)
+    return service
+
+
 def search_file_by_name(drive_service, file_name):
     print('searching file name {} in gdrive...'.format(file_name))
     page_token = None
@@ -41,7 +68,7 @@ def download_file(drive_service, file_name, path_to_download):
     fh = io.FileIO(path_to_download, 'wb')
     downloader = MediaIoBaseDownload(fh, request)
     done = False
-    
+
     try:
         while done is False:
             status, done = downloader.next_chunk()
@@ -51,29 +78,7 @@ def download_file(drive_service, file_name, path_to_download):
 
 
 def main():
-    """Shows basic usage of the Drive v3 API.
-    Prints the names and ids of the first 10 files the user has access to.
-    """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-
-    service = build('drive', 'v3', credentials=creds)
+    service = authenticate()
 
     # Call the Drive v3 API
     results = service.files().list(
