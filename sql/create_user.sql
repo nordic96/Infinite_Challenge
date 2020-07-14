1c692fdc298998e6afaca01a2a905758d1f428dd
Gi Hun Ko <rhrlgns96@gmail.com>
2020-07-14 22:43:37 +0800

    removing create_user sql for security reasons

--- sql/create_user.sql
+++
@@ -1,9 +0,0 @@
-USE Infinite_Challenge;
-CREATE LOGIN db_user WITH PASSWORD = 'infinite';
-CREATE USER db_user FOR LOGIN db_user WITH DEFAULT_SCHEMA = dbo;
-
-Use Infinite_Challenge go
-GRANT SELECT, INSERT, UPDATE ON SCHEMA :: dbo TO db_user;
-
-Use master go
-GRANT ADMINISTER BULK OPERATIONS TO db_user;
