574516d258ab8ef0626c0c7778f839958243ed91
briyanii <e0310729@u.nus.edu>
2020-07-17 02:34:13 +0800

    Refactored instance methods to use attributes instead of config outside of __init__ method

--- src/pipeline/phase1.py
+++ src/pipeline/phase1.py
@@ -1,5 +1,7 @@
 import os
+import sys
 import shutil
+import configparser
 import cv2
 from tempfile import TemporaryDirectory
 from src.logger.result_logger import ResultLogger
@@ -12,17 +14,9 @@ from src.utils.gdrivefile_util import GDrive
 # 2. cache images with skulls
 # 3. update result.csv for each image
 class Phase1:
-    def __init__(self, config):
+    def __init__(self, config, episode_number):
         logger.info('Initializing phase 1 parameters')
-        self.config = config
-        try:
-            self.config['episode_number'] = os.environ['IC_EPISODE_NUMBER']
-            self.config['azure_key'] = os.environ['IC_AZURE_KEY_SKULL']
-            self.config['token_path'] = os.environ['IC_GDRIVE_AUTH_TOKEN_PATH']
-            self.config['client_secrets_path'] = os.environ['IC_GDRIVE_CLIENT_SECRETS_PATH']
-        except KeyError as ex:
-            logger.error('Missing required environment variable')
-            raise ex
+        self.episode_number = episode_number
 
         # prepare directory for caching
         self.cache_dir = TemporaryDirectory()
@@ -32,69 +26,81 @@ class Phase1:
         self.result_logger = ResultLogger(self.result_cache)
         # prepare directory for local saving
         if config.getboolean('save_images') or config.getboolean('save_results'):
-            self.config['save_cached_files'] = 'True'
-            out_dir_path = os.path.join(config['output_directory_path'], f'episode{config["episode_number"]}')
+            self.save_images = config.getboolean('save_images')
+            self.save_results = config.getboolean('save_results')
+            out_dir_path = os.path.join(config['output_directory_path'], f'episode{self.episode_number}')
             if not os.path.exists(out_dir_path):
                 os.makedirs(out_dir_path, exist_ok=True)
-            self.config['output_directory_path'] = out_dir_path
-        self.gdrive = GDrive(token_path=config['token_path'], client_secrets_path=config['client_secrets_path'])
+            self.output_directory_path = out_dir_path
+        # for uploading cached files
         if config.getboolean('upload_labelled') or config.getboolean('upload_unlabelled') \
                 or config.getboolean('upload_results'):
-            self.config['upload_cached_files'] = 'True'
+            self.upload_unlabelled = config.getboolean('upload_unlabelled')
+            self.upload_labelled = config.getboolean('upload_labelled')
+            self.upload_results = config.getboolean('upload_results')
+        # for video processing
+        self.display = config.getboolean('display')
+        self.video_sample_rate = config.getint('video_sample_rate')
+        self.skull_confidence_threshold = config.getfloat('skull_confidence_threshold')
+        self.skull_model_version = config['skull_model_version']
+        try:
+            self.azure_key = os.environ['IC_AZURE_KEY_SKULL']
+        except KeyError as ex:
+            logger.error('Missing required environment variable')
+            raise ex
+        # for google drive
+        self.gdrive = GDrive(token_path=os.environ['IC_GDRIVE_AUTH_TOKEN_PATH'],
+                             client_secrets_path=os.environ['IC_GDRIVE_CLIENT_SECRETS_PATH'])
 
     def download_episode(self):
-        config = self.config
-        episode_filename = f'episode{config["episode_number"]}.mp4'
+        episode_filename = f'episode{self.episode_number}.mp4'
         remote_path = os.path.join('episodes', episode_filename)
         cached_video_path = os.path.join(self.cache_dir.name, episode_filename)
         self.gdrive.download_file(remote_path, cached_video_path)
         return cached_video_path
 
     def process_episode(self, episode_filepath):
-        config = self.config
         extracted_frames = vr.process_stream(
             video_path=episode_filepath,
-            azure_key=config['azure_key'],
-            confidence=float(config['skull_confidence_threshold']),
-            model_version=config['skull_model_version'],
-            sample_rate=int(config['video_sample_rate']),
-            display=config.getboolean('display')
+            azure_key=self.azure_key,
+            confidence=self.skull_confidence_threshold,
+            model_version=self.skull_model_version,
+            sample_rate=self.video_sample_rate,
+            display=self.display
         )
         return extracted_frames
 
     def cache_extracted_frames(self, extracted_frames):
-        config = self.config
         for frame in extracted_frames:
-            filename = f"{config['episode_number']}_{frame.timestamp.with_delimiter('_')}.jpg"
+            filename = f"{self.episode_number}_{frame.timestamp.with_delimiter('_')}.jpg"
             dst_path = os.path.join(self.cache_dir.name, filename)
             cv2.imwrite(dst_path, frame.frame)
-            lfilename = f"{config['episode_number']}_{frame.timestamp.with_delimiter('_')}_skull.jpg"
+            lfilename = f"{self.episode_number}_{frame.timestamp.with_delimiter('_')}_skull.jpg"
             dst_path = os.path.join(self.cache_dir.name, lfilename)
             cv2.imwrite(dst_path, frame.labelled_frame)
 
     def update_results(self, extracted_frames):
         for frame in extracted_frames:
-            self.result_logger.add_skull_entry(self.config['episode_number'], frame.timestamp, frame.coord)
+            self.result_logger.add_skull_entry(self.episode_number, frame.timestamp, frame.coord)
 
-    def upload_cached_files(self, upload_unlabelled=True, upload_labelled=True, upload_results=True):
+    def upload_cached_files(self):
         dir_path = self.cache_dir.name
-        dst_dir = f'episode{self.config["episode_number"]}_output'
+        dst_dir = f'episode{self.episode_number}_output'
         for file in os.listdir(dir_path):
             path = os.path.join(dir_path, file)
-            if file == 'results.csv' and upload_results:
+            if file == 'results.csv' and self.upload_results:
                 self.gdrive.upload_file(path, remote_filepath=os.path.join(dst_dir, 'phase1_results.csv'))
-            elif file.endswith('skull.jpg') and upload_labelled:
+            elif file.endswith('skull.jpg') and self.upload_labelled:
                 self.gdrive.upload_file(path, remote_filepath=os.path.join(dst_dir, file))
-            elif file.endswith('.jpg') and not file.endswith('skull.jpg') and upload_unlabelled:
+            elif file.endswith('.jpg') and not file.endswith('skull.jpg') and self.upload_unlabelled:
                 self.gdrive.upload_file(path, remote_filepath=os.path.join(dst_dir, file))
 
-    def save_cached_files(self, save_images=True, save_results=True):
-        config = self.config
-        out_dir_path = config['output_directory_path']
+    def save_cached_files(self):
+        out_dir_path = self.output_directory_path
         if not os.path.isdir(out_dir_path):
             raise FileNotFoundError(f'The specified output path is not a directory: {out_dir_path}')
         for file in os.listdir(self.cache_dir.name):
-            if (file.endswith('.jpg') and save_images) or (file == 'results.csv' and save_results):
+            if (file.endswith('.jpg') and self.save_images) or (file == 'results.csv' and self.save_results):
                 dst = os.path.join(out_dir_path, file)
                 dst = os.path.abspath(dst)
                 logger.info(f'Saving {file} to {dst}')
@@ -103,35 +109,22 @@ class Phase1:
     def run(self):
         try:
             logger.info('Phase 1 start')
-            config = self.config
+            ep_no = self.episode_number
             # get episode from google drive
-            logger.info(f'Downloading {config["episode_number"]} from Google Drive')
+            logger.info(f'Downloading episode {ep_no} from Google Drive')
             episode_filepath = self.download_episode()
             # process episode
-            logger.info(f'Finding frames with skulls in {config["episode_number"]}')
+            logger.info(f'Finding frames with skulls in episode {ep_no}')
             extracted_frames = self.process_episode(episode_filepath)
             # update results and cache image locally on container
-            logger.info(f'Caching frames with skulls in {config["episode_number"]}')
+            logger.info(f'Caching frames with skulls in episode {ep_no}')
             self.cache_extracted_frames(extracted_frames)
 
             logger.info(f'Updating results CSV file')
             self.update_results(extracted_frames)
 
-            if config.getboolean('upload_cached_files'):
-                up_unlabelled = config.getboolean('upload_unlabelled')
-                up_labelled = config.getboolean('upload_labelled')
-                up_res = config.getboolean('upload_results')
-                logger.info(f'Uploading cached files: '
-                            f'unlabelled_images={up_unlabelled}, labelled_images={up_labelled}, results={up_res}')
-                self.upload_cached_files(upload_unlabelled=up_unlabelled,
-                                         upload_labelled=up_labelled,
-                                         upload_results=up_res)
-
-            if config.getboolean('save_cached_files'):
-                save_imgs = config.getboolean('save_images')
-                save_res = config.getboolean('save_results')
-                logger.info(f'Saving cached files: unlabelled_images={save_imgs}, results={save_res}')
-                self.save_cached_files(save_images=save_imgs, save_results=save_res)
+            self.upload_cached_files()
+            self.save_cached_files()
 
             logger.info('Phase 1 complete')
         except Exception as ex:
@@ -140,8 +133,7 @@ class Phase1:
 
 
 if __name__ == '__main__':
-    import configparser
     config = configparser.ConfigParser()
-    config.read('../../strings.ini')
-    p1 = Phase1(config['Phase1'])
+    config.read(sys.argv[2])
+    p1 = Phase1(config['Phase1'], sys.argv[3])
     p1.run()
--- src/pipeline/phase2.py
+++ src/pipeline/phase2.py
@@ -1,5 +1,7 @@
 import os
+import sys
 import shutil
+import configparser
 from tempfile import TemporaryDirectory
 from src.model import azure_face_recognition as afr
 from src.model.vid_recognition import Timestamp
@@ -9,70 +11,62 @@ from src.utils.gdrivefile_util import GDrive
 
 
 class Phase2:
-    def __init__(self, config):
+    def __init__(self, config, episode_number):
         logger.info('Initializing phase 2 parameters')
-        self.config = config
-        try:
-            self.config['episode_number'] = os.environ['IC_EPISODE_NUMBER']
-            self.config['azure_key'] = os.environ['IC_AZURE_KEY_FACE']
-            self.config['token_path'] = os.environ['IC_GDRIVE_AUTH_TOKEN_PATH']
-            self.config['client_secrets_path'] = os.environ['IC_GDRIVE_CLIENT_SECRETS_PATH']
-        except KeyError as ex:
-            logger.error('Missing required environment variable')
-            raise ex
+        self.episode_number = episode_number
         # input
-        self.config['input_directory_path'] = os.path.join(config['input_directory_path'],
-                                                           f'episode{config["episode_number"]}')
+        self.input_directory_path = os.path.join(config['input_directory_path'],  f'episode{episode_number}')
         # prepare directory for caching
         self.cache_dir = TemporaryDirectory()
         self.result_cache = os.path.join(self.cache_dir.name, 'results.csv')
-        self.config['results_file_path'] = os.path.join(self.config['input_directory_path'], 'results.csv')
-        shutil.copy(self.config['results_file_path'], self.result_cache)
+        results_file_path = os.path.join(self.input_directory_path, 'results.csv')
+        shutil.copy(results_file_path, self.result_cache)
         self.result_logger = ResultLogger(self.result_cache)
         # prepare directory for local saving
-        if config.getboolean('save_images') or config.getboolean('save_results'):
-            self.config['save_cached_files'] = 'True'
-            out_dir_path = os.path.join(config['output_directory_path'], f'episode{config["episode_number"]}')
+        self.save_images = config.getboolean('save_images')
+        self.save_results = config.getboolean('save_results')
+        if self.save_images or self.save_results:
+            out_dir_path = os.path.join(config['output_directory_path'], f'episode{self.episode_number}')
             if not os.path.exists(out_dir_path):
                 os.makedirs(out_dir_path, exist_ok=True)
-            self.config['output_directory_path'] = out_dir_path
-        else:
-            self.config['save_cached_files'] = 'False'
-        self.gdrive = GDrive(token_path=config['token_path'], client_secrets_path=config['client_secrets_path'])
-        if config.getboolean('upload_images') or config.getboolean('upload_results'):
-            self.config['upload_cached_files'] = 'True'
-        else:
-            self.config['upload_cached_files'] = 'False'
+            self.output_directory_path = out_dir_path
+        # for uploading cached files
+        self.upload_labelled = config.getboolean('upload_labelled')
+        self.upload_results = config.getboolean('upload_results')
+        # for google drive
+        self.gdrive = GDrive(token_path=os.environ['IC_GDRIVE_AUTH_TOKEN_PATH'],
+                             client_secrets_path=os.environ['IC_GDRIVE_CLIENT_SECRETS_PATH'])
+        # for face recognition
+        self.person_group_id = config['person_group_id']
+        self.faceclient = afr.authenticate_client(config['endpoint'], os.environ['IC_AZURE_KEY_FACE'])
 
-    def upload_cached_files(self, upload_labelled=True, upload_results=True):
+    def upload_cached_files(self):
         dir_path = self.cache_dir.name
-        dst_dir = f'episode{self.config["episode_number"]}_output'
+        dst_dir = f'episode{self.episode_number}_output'
         for file in os.listdir(dir_path):
             path = os.path.join(dir_path, file)
-            if file == 'results.csv' and upload_results:
+            if file == 'results.csv' and self.upload_results:
                 self.gdrive.upload_file(path, remote_filepath=os.path.join(dst_dir, 'phase2_results.csv'))
-            elif file.endswith('face.jpg') and upload_labelled:
+            elif file.endswith('face.jpg') and self.upload_labelled:
                 self.gdrive.upload_file(path, remote_filepath=os.path.join(dst_dir, file))
 
-    def save_cached_files(self, save_images=True, save_results=True):
-        config = self.config
-        out_dir_path = config['output_directory_path']
+    def save_cached_files(self):
+        out_dir_path = self.output_directory_path
         if not os.path.isdir(out_dir_path):
             raise FileNotFoundError(f'The specified output path is not a directory: {out_dir_path}')
         for file in os.listdir(self.cache_dir.name):
-            if (file.endswith('.jpg') and save_images) or (file == 'results.csv' and save_results):
+            if (file.endswith('.jpg') and self.save_images) or (file == 'results.csv' and self.save_results):
                 dst = os.path.join(out_dir_path, file)
                 dst = os.path.abspath(dst)
                 logger.info(f'Saving {file} to {dst}')
                 shutil.move(os.path.join(self.cache_dir.name, file), dst)
 
     def process_images(self, image_paths):
-        fc = afr.authenticate_client(self.config['endpoint'], self.config['azure_key'])
         mappings = {}
         for path in image_paths:
             in_dir_path, filename = os.path.split(path)
             logger.info(f'Processing {filename}')
-            faces = afr.recognise_faces(fc, path, self.config['person_group_id'])
+            faces = afr.recognise_faces(self.faceclient, path, self.person_group_id)
             mappings[filename] = faces
             logger.info(f'Caching labelled images')
             name, ext = filename.split('.')
@@ -95,29 +89,20 @@ class Phase2:
             self.result_logger.update_face_entry(ep, Timestamp(h, m, s, ms), bounding_boxes, names)
 
     def get_imagepaths(self):
-        in_dir_path = self.config['input_directory_path']
+        in_dir_path = self.input_directory_path
         paths = [os.path.join(in_dir_path, filename) for filename in os.listdir(in_dir_path)]
         return list(filter(lambda x: x.endswith('.jpg') and not x.endswith('_skull.jpg'), paths))
 
     def run(self):
         try:
             logger.info('Phase 2 start')
-            config = self.config
             paths = self.get_imagepaths()
             logger.info('Processing images in input directory')
-            up_results = self.process_images(paths)
+            results = self.process_images(paths)
             logger.info('Updating result CSV file')
-            self.update_results(up_results)
-            if config['upload_cached_files']:
-                up_labelled = config['upload_images']
-                up_results = config['upload_results']
-                logger.info(f'Uploading cached files: labelled_images={up_labelled}, results={up_results}')
-                self.upload_cached_files(upload_labelled=up_labelled, upload_results=up_results)
-            if config['save_cached_files']:
-                save_labelled = config['save_images']
-                save_results = config['save_results']
-                logger.info(f'Saving cached files: labelled_images={save_labelled}, results={save_results}')
-                self.save_cached_files(save_images=save_labelled, save_results=save_results)
+            self.update_results(results)
+            self.upload_cached_files()
+            self.save_cached_files()
             logger.info('Phase 2 complete')
         except Exception as ex:
             logger.error('Phase 2 failed')
@@ -125,8 +110,7 @@ class Phase2:
 
 
 if __name__ == '__main__':
-    import configparser
     config = configparser.ConfigParser()
-    config.read('../strings.ini')
-    p1 = Phase2(config['Phase2'])
+    config.read(sys.argv[2])
+    p1 = Phase2(config['Phase2'], sys.argv[3])
     p1.run()
