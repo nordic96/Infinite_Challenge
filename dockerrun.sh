ff11afbb93e3ab9f354cf8d5a0c064acda36b62f
briyanii <e0310729@u.nus.edu>
2020-07-22 01:16:30 +0800

    Modify dockerrun.sh and docker_entrypoint.sh to match change to phase1, 2, and 3 input arguement

--- dockerrun.sh
+++ dockerrun.sh
@@ -2,7 +2,7 @@
 
 HOST_DIRECTORY= # abs path of directory containing external files (credentials.json, token.pickle, config file)
 CONFIG_FILENAME=default.ini  #file name of config file to use, should be in the root directory of the specified host directory
-EPISODE= # episode number
+EPISODE_FILENAME= # episode filename (e.g. episode1.mp4, or episode231.avi)
 DOCKER_IMAGE= #docker image
 
 docker run \
@@ -13,4 +13,4 @@ docker run \
   -e IC_GDRIVE_AUTH_TOKEN_PATH=/external/token.pickle \
   -e IC_GDRIVE_CLIENT_SECRETS_PATH=/external/credentials.json \
   --mount type=bind,source=$HOST_DIRECTORY,target=/external \
-  $DOCKER_IMAGE /external/$CONFIG_FILENAME $EPISODE
+  $DOCKER_IMAGE /external/$CONFIG_FILENAME $EPISODE_FILENAME
